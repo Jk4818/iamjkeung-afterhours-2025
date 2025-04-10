1f570daa7b8545d2062e0d358a174ced65b37630
Jason Keung <work.jk4818@gmail.com>
2025-04-10 14:40:17 +0100

    Feat: full repsonsiveness, added video

--- src/components/GettingStarted.tsx
+++ src/components/GettingStarted.tsx
@@ -4,25 +4,25 @@ type Props = {}
 
 export default function GettingStarted({ }: Props) {
     return (
-        <div className='bg-main-green w-full h-96 p-4 lg:p-10 flex justify-between items-center text-xl'>
-            <div className='flex flex-col w-1/2 h-full  justify-between'>
+        <div className='bg-main-green w-full h-96 p-4 lg:p-10 flex flex-col lg:flex-row justify-between items-center text-xl'>
+            <div className='flex flex-col w-full lg:w-1/2 h-full  justify-evenly'>
                 <div className='w-full md:w-3/4 h-1/2 text-sm lg:text-md'>
                     <p>
                     We're a tight-knit collective of <span className='font-black'>creatives, producers, and music lovers</span> building a space where artists can feel seen, heard, and supported. After Hours is powered by <a className='font-black' href="iamjkeung.com" target='_blank'>iamjkeung.com</a>—a passion project rooted in storytelling, collaboration, and thoughtful production. We work closely with each artist to shape something meaningful, not just marketable.
                     </p>
                 </div>
-                <div className='h-1/2 flex items-end'>
+                <div className='h-max lg:h-1/2 flex items-end'>
                     <h4 className='font-black'>Want to perform with us?</h4>
                 </div>
             </div>
-            <div className='flex w-3/4 h-full items-end justify-end font-serif text-foreground'>
+            <div className='flex w-full lg:w-3/4 h-max lg:h-full items-end justify-end font-serif text-foreground'>
                     <h1
                         className='pr-2 text-4xl md:text-7xl xl:text-9xl font-bold'
                     >
                         get started
                     </h1>
-                    <div className='h-max self-end md:mb-4 xl:mb-8'>
-                        <div className={`dot w-6 h-6 rounded-full  bg-foreground`}></div>
+                    <div className='h-max self-end mb-2.5 md:mb-4 xl:mb-8'>
+                        <div className={`dot w-5 h-5 md:w-6 md:h-6 rounded-full  bg-foreground`}></div>
                     </div>
                 </div>
         </div>
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,6 +4,12 @@ import AnimatedLogo from './AnimatedLogo'
 type Props = {}
 
 export default function LandingPage({ }: Props) {
+
+
+    const videoSrc = "https://d1jtup13qecgqg.cloudfront.net/assets/after-hours/hero-hd.mp4";
+    const videoThumb = "https://d1jtup13qecgqg.cloudfront.net/assets/after-hours/hero-hd-thumb.jpg";
+
+
     return (
         <div className='bg-main-blue w-full h-screen p-4 md:p-10'>
             <div className='absolute left-0  mx-4 md:mx-10 self-start '>
@@ -15,10 +21,31 @@ export default function LandingPage({ }: Props) {
                         <h1 className=''>
                             A cozy, late-night performance series where artistry takes the lead.
                         </h1>
-                        <h1 className='my-10 sm:my-0 mt-4 w-full'>Real music. Real moments.</h1>
+                        <h1 className='my-7 sm:my-0 mt-4 w-full'>Real music. Real moments.</h1>
                     </div>
                 </div>
-                <div className='w-full lg:w-6/13 h-full bg-blue-300 rounded-xl'></div>
+                <div className='w-full lg:w-6/13 h-full rounded-xl'>
+                    <video
+                        loop
+                        autoPlay
+                        muted
+                        poster={videoThumb}
+                        className="w-full h-full object-cover rounded-xl"
+                    >
+                        <source src={videoSrc} type="video/mp4" />
+                    </video>
+                </div>
+            </div>
+            {/* Scroll Indicator */}
+            <div
+                className="hidden lg:block absolute bottom-20 left-0 right-0 text-center cursor-pointer"
+            >
+                <p className="text-xs font-black text-foreground">Scroll down</p>
+                <div
+                    className="mt-2"
+                >
+                    ↓
+                </div>
             </div>
         </div>
     )
